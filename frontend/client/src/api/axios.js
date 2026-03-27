bea75b998c07ec0f57153eabdf73f00c463026ab
ypandey9 <128962086+ypandey9@users.noreply.github.com>
2026-03-27 16:20:57 +0000

    connected front/backend

--- frontend/client/src/api/axios.js
+++ frontend/client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API =axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://miniature-winner-7v76w6rv5q4hrgjr-5000.app.github.dev/api",
 });
 
 //Attach token automatically 
